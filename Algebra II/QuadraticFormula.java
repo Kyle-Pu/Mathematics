dac6840dd8454c75e33b412c287901a9d243d9aa
Daniel Binoy <bleeblob43@gmail.com>
2018-03-14 16:02:08 -0700

    Update QuadraticFormula.java

--- Algebra II/QuadraticFormula.java
+++ Algebra II/QuadraticFormula.java
@@ -25,8 +25,16 @@ public class QuadraticFormula {
 		double answer2 = (double) (((b) - Math.sqrt(b * b - 4*a*c))) / (2 * a);
 
     //Print out zeroes calculated from the quadratic formula
-		System.out.println("Answer 1: " + answer1);
-		System.out.println("Answer 2: " + answer2);
+		if(!Double.isNan(answer1)){
+			System.out.println("Answer 1: " + answer1);	
+		}else{
+			System.out.println("imaginary answer");
+		}
+		if(!Double.isNan(answer2)){
+			System.out.println("Answer 2: " + answer2);	
+		}else{
+			System.out.println("imaginary answer");
+		}
 
     //Close the scanner to prevent any leaks
 		scan.close();
