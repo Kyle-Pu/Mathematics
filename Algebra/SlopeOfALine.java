e1f7cf255e3b0c96542de313f7e74eb5fedd5ee5
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-03-18 15:52:22 -0700

    Change some formatting
    Everything looks great! Just changed some formatting.

--- Algebra/SlopeOfALine.java
+++ Algebra/SlopeOfALine.java
@@ -3,24 +3,24 @@ import java.util.*;
 public class SlopeOfALine {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Slope of a line Calculator!");
+        System.out.println("Welcome to the Slope of a Line Calculator!");
 
         try {
             Scanner scan = new Scanner(System.in);
             //Prompt user for point values
 
             //The First Point
-            System.out.print("What is the value of 'x' coordinate of your first point?  ");
+            System.out.print("What is the value of the 'x' coordinate of your first point?  ");
             double x1 = scan.nextDouble();
 
-            System.out.print("What is the value of 'y' coordinate of your first point?  ");
+            System.out.print("What is the value of the 'y' coordinate of your first point?  ");
             double y1 = scan.nextDouble();
 
             //The Second Point
-            System.out.print("What is the value of 'x' coordinate of your second point?  ");
+            System.out.print("What is the value of the 'x' coordinate of your second point?  ");
             double x2 = scan.nextDouble();
 
-            System.out.print("What is the value of 'y' coordinate of your second point?  ");
+            System.out.print("What is the value of the 'y' coordinate of your second point?  ");
             double y2 = scan.nextDouble();
 
 
@@ -32,7 +32,7 @@ public class SlopeOfALine {
             }else {
                 //Calculate the slope of a line
                 double slope = (y2 - y1) / (x2 - x1);
-                answer = "The slope is "+slope;
+                answer = "The slope of your line is "+slope;
             }
             //Print out the slope value
             System.out.println("\n--------------");
@@ -42,7 +42,7 @@ public class SlopeOfALine {
             scan.close();
         } catch (InputMismatchException ex) {
 
-            System.err.println("Please provide only numbers and not text, please restart the program again");
+            System.err.println("Please provide only numbers (not text). Please restart the program and try again!);
         }
 
     }
