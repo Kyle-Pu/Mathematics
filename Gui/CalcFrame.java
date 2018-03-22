ddbcd92dfc21bca9bb95d17f666b32b41aa2788d
Daniel Binoy <dbinoy15@gmail.com>
2018-03-21 17:35:26 -0700

    recieve input
    the text field now recieves input and prints to system.out when the enter key is pressed

--- Gui/CalcFrame.java
+++ Gui/CalcFrame.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public  class CalcFrame extends JFrame{
 
@@ -13,8 +14,15 @@ public  class CalcFrame extends JFrame{
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setSize(1000, 1000);
     this.add((panel = new CalcPanel(handler)));
+
     inputBox = new JTextField(20);
-    inputBox.addActionListener(this);
+    Action action = new AbstractAction(){
+      @Override
+      public void actionPerformed(ActionEvent e){
+        System.out.println(inputBox.getText());
+      }
+    };
+    inputBox.addActionListener(action);
     panel.add(inputBox);
     this.setVisible(true);
     this.handler = handler;
