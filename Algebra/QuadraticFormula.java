6f44436e8e4162f0dff86519f104054b0d734375
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-03-29 22:13:50 -0700

    Update QuadraticFormula.java
    Fix formatting issues and added restart functionality to the program!

--- Algebra/QuadraticFormula.java
+++ Algebra/QuadraticFormula.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class QuadraticFormula {
 
@@ -8,8 +8,8 @@ public class QuadraticFormula {
 
 		Scanner scan = new Scanner(System.in);
 
-    //Prompt user for coefficients in polynomial and store values
-		System.out.print("What is the value of 'a' of your polynomial in the form of ax^2 + bx + c?");
+		// Prompt user for coefficients in polynomial and store values
+		System.out.print("What is the value of 'a' of your polynomial in the form of ax^2 + bx + c? ");
 		double a = scan.nextDouble();
 
 		System.out.print("What is the value of 'b' of your polynomial? ");
@@ -20,23 +20,38 @@ public class QuadraticFormula {
 
 		System.out.println("Calculating...");
 
-    //Calculate both forms of the quadratic formula (plus or minus in the numerator)
-		double answer1 = (double) (((b) + Math.sqrt(b * b - 4*a*c))) / (2 * a);
-		double answer2 = (double) (((b) - Math.sqrt(b * b - 4*a*c))) / (2 * a);
+		// Calculate both forms of the quadratic formula (plus or minus in the
+		// numerator)
+		double answer1 = (double) (((b) + Math.sqrt(b * b - 4 * a * c))) / (2 * a);
+		double answer2 = (double) (((b) - Math.sqrt(b * b - 4 * a * c))) / (2 * a);
 
-    //Print out zeroes calculated from the quadratic formula
-		if(!Double.isNan(answer1)){
-+			System.out.println("Answer 1: " + answer1);	
-+		}else{
-+			System.out.println("Imaginary solution...");
-+		}
-+		if(!Double.isNan(answer2)){
-+			System.out.println("Answer 2: " + answer2);	
-+		}else{
-+			System.out.println("Imaginary solution...");
-+		}
+		// Print out zeroes calculated from the quadratic formula
+		if (!Double.isNaN(answer1)) {
+			System.out.println("Answer 1: " + answer1);
+		} else {
+			System.out.println("Imaginary solution...");
+		}
+		if (!Double.isNaN(answer2)) {
+			System.out.println("Answer 2: " + answer2);
+		} else {
+			System.out.println("Imaginary solution...");
+		}
 
-    //Close the scanner to prevent any leaks
+		System.out.println();
+
+		// Restart the program is user would like to
+
+		System.out.println(
+				"Thank you for using our calculator! If you would like to restart this program, enter in the 'Y' key with your keyboard and hit \"Enter\"!");
+		scan.nextLine();
+
+		String restart = scan.nextLine();
+
+		if (restart.toLowerCase().equals("y")) {
+			main(new String[0]);
+		}
+
+		// Close the scanner to prevent any leaks
 		scan.close();
 
 	}
