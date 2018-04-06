4381ee37ef57a1ae0ee984dfd32347d772287e80
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-04-06 08:59:04 -0700

    Add coordinates file

---
+++ Graphing Calculator/Coordinate.java
@@ -0,0 +1,29 @@
+package GraphingCalculator;
+
+public class Coordinate {
+
+	private double x;
+	private double y;
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+}
