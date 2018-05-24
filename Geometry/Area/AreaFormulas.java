1301bced562b32dc0778664e32f805ad4890d7f5
Sarah Cohen <cohen.sarah591@gmail.com>
2018-05-24 18:18:06 -0400

    added AreaFormulas class for the GUI

---
+++ Geometry/Area/AreaFormulas.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * This class contains methods that return the area's of different shapes. 
+ * @author scohen40
+ */
+public class AreaFormulas {
+
+	public static double rectangle(double height, double width) {
+		return height * width;
+	}
+
+	public static double circle(double radius) {
+		return radius * radius * Math.PI;
+	}
+
+	public static double triangle(double length, double height) {
+		return 0.5 * length * height;
+	}
+
+	public static double square(double side) {
+		return side * side;
+	}
+}
