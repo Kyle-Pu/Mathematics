f97a487def99f19f5f40ca0bd3deba6cfd56266a
Josh Wheeler <josh.wheeler@alertinnovation.com>
2019-11-06 20:33:05 -0500

    Moved existing files into a Maven project. Added the following items to be ignored by git (non-project related): VisualStudioCode, SublimeText, Eclipse, Windows, archives, Emacs, Microsoft Office temporary files, macOS, Java, JetBrains IDEs, Xcode, VisualStudio IDE, Maven, images.

--- Algebra/Intercepts/ZeroSlopeException.java
+++
@@ -1,4 +0,0 @@
-
-public class ZeroSlopeException extends Throwable {
-
-}
---
+++ src/main/java/GUI/CalcMain.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+		new CalcHandler();
+	}
+}
