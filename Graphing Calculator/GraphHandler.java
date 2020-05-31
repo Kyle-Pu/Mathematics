a39a0a93936446d57b023f6bff2efa1c3d82bec5
fnffn123 <37815222+fnffn123@users.noreply.github.com>
2020-05-31 22:55:45 +0900

    refactring GraphHandler(extract method)
    1. refactoring operations : extract method
    2. refactoring method: printDirections, getFormula, getLeftBound, getRightBound, getStep, getCoordinates, replaceWithInput, addCoordinate, changeToString, repaintCanvas, getNextChar, unexpectedCharError, getExponentiation
    3. reason : to improve code understandability, to improve modifiability

--- Graphing Calculator/GraphHandler.java
+++ Graphing Calculator/GraphHandler.java
@@ -13,48 +13,99 @@ public class GraphHandler {
 		coordinates = new ArrayList<Coordinate>();
 		frame = new GraphFrame(this);
 		graphingCalculator();
+		newThreadStart();
+	}
+
+	private void newThreadStart() {
 		((Thread) (thread = new GraphThread(this))).start();
 	}
 
 	//Fills the coordinates ArrayList with points based on a function
 	public void graphingCalculator(){
 		
+		printDirections();
+
+		
+		String formula = getFormula();
+		String revisedFormula = formula; //For swapping in new input values each iteration to calculate coordinates
+		
+		double leftBound = getLeftBound();
+
+		double rightBound = getRightBound();
+
+		double step = getStep();
+
+		
+		getCoordinates(formula, leftBound, rightBound, step);
+		
+		getScale(leftBound, rightBound);
+		
+		}
+
+	private void getCoordinates(String formula, double leftBound, double rightBound, double step) {
+		String revisedFormula;
+		for (double i = leftBound; i <= rightBound; i += step) {
+					String input = changeToString(i); 
+					revisedFormula = replaceWithInput(formula, input); 
+					addCoordinate(revisedFormula, i); 
+		}
+	}
+
+	private String replaceWithInput(String formula, String input) {
+		return formula.replaceAll("x", "(" + input + ")");
+	}
+
+	private void addCoordinate(String revisedFormula, double i) {
+		coordinates.add(new Coordinate(i, evaluate(revisedFormula)));
+	}
+
+	private String changeToString(double i) {
+		return Double.toString(i);
+	}
+
+	private double getStep() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Step of your function (the increments at which the function is evaluated at: ");
+		double step = scan.nextDouble();
+		scan.close();
+		return step;
+	}
+
+	private String getFormula() {
+		Scanner scan = new Scanner(System.in);
+		String formula = scan.nextLine(); //The formula
+		scan.close();
+		return formula;
+	}
+
+	private double getRightBound() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Right bound of your domain? ");
+		double rightBound = scan.nextDouble();
+		scan.close();
+		System.out.println();
+		return rightBound;
+	}
+
+	private double getLeftBound() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println();
+
+		System.out.print("Left bound of your domain? ");
+		double leftBound = scan.nextDouble();
+		System.out.println();
+		scan.close();
+		return leftBound;
+	}
+
+	private void printDirections() {
 		System.out.println(
 				"Welcome to our graphing calcalculator!");
 		System.out.println();
 		System.out.println("Diections: When multiplying terms, always use the multiplication (*). Ex: 2x^2 should be 2*x^2");
 		System.out.println();
 		System.out.print("Please insert your equation in terms of x. Separate each term with a space (Ex: x^2 + 2*x + 3  \n You MUST include all signs (2x should be 2*x)): \ny = ");
-
-		Scanner scan = new Scanner(System.in);
-		
-		String formula = scan.nextLine(); //The formula
-		String revisedFormula = formula; //For swapping in new input values each iteration to calculate coordinates
-		
-		System.out.println();
-
-		System.out.print("Left bound of your domain? ");
-		double leftBound = scan.nextDouble();
-		System.out.println();
-
-		System.out.print("Right bound of your domain? ");
-		double rightBound = scan.nextDouble();
-		System.out.println();
-
-		System.out.print("Step of your function (the increments at which the function is evaluated at: ");
-		double step = scan.nextDouble();
-		scan.close();
-		
-		//Algorithm to calculate coordinates
-		for (double i = leftBound; i <= rightBound; i += step) {
-					String input = Double.toString(i); //Take the input at the given point based on left bound, right bound, and step of the function
-					revisedFormula = formula.replaceAll("x", "(" + input + ")"); //Take the original formula and replace all occurrences of 'x' with the input //Take the original formula and replace all occurrences of 'x' with the input
-					coordinates.add(new Coordinate(i, evaluate(revisedFormula))); // Add a new coordinate to the coordinates ArrayList with data points
-			}
-		
-		getScale(leftBound, rightBound);
-		
-		}
+	}
 
 	
 	public void getScale(double leftBound, double rightBound) {
@@ -62,24 +113,30 @@ public class GraphHandler {
 	}
 	
 	
-	//The thread will run this update function which repaints the GraphPanel
 	public void updateGraphPanel() {
-		// Paint canvas
+		repaintCanvas();
+	}
+
+	private void repaintCanvas() {
 		getGraphFrame().getGraphPanel().repaint();
 	}
 	
 	//Author: https://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
 		private static double evaluate(final String mathExpression) {
 		    return new Object() {
-		        int pos = -1, ch;
+		        int position = -1, aChar;
 
 		        void nextChar() {
-		            ch = (++pos < mathExpression.length()) ? mathExpression.charAt(pos) : -1;
+		            aChar = getNextChar(mathExpression);
 		        }
 
+				private int getNextChar(final String mathExpression) {
+					return (++position < mathExpression.length()) ? mathExpression.charAt(position) : -1;
+				}
+
 		        boolean eat(int charToEat) {
-		            while (ch == ' ') nextChar();
-		            if (ch == charToEat) {
+		            while (aChar == ' ') nextChar();
+		            if (aChar == charToEat) {
 		                nextChar();
 		                return true;
 		            }
@@ -88,9 +145,10 @@ public class GraphHandler {
 
 		        double parse() {
 		            nextChar();
-		            double x = parseExpression();
-		            if (pos < mathExpression.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-		            return x;
+		            double result = parseExpression();
+		            if (position < mathExpression.length())
+						unexpectedCharError();
+		            return result;
 		        }
 
 		        // Grammar:
@@ -100,20 +158,20 @@ public class GraphHandler {
 		        //        | number | functionName factor | factor `^` factor
 
 		        double parseExpression() {
-		            double x = parseTerm();
+		            double result = parseTerm();
 		            for (;;) {
-		                if      (eat('+')) x += parseTerm(); // addition
-		                else if (eat('-')) x -= parseTerm(); // subtraction
-		                else return x;
+		                if      (eat('+')) result += parseTerm(); // addition
+		                else if (eat('-')) result -= parseTerm(); // subtraction
+		                else return result;
 		            }
 		        }
 
 		        double parseTerm() {
-		            double x = parseFactor();
+		            double result = parseFactor();
 		            for (;;) {
-		                if      (eat('*')) x *= parseFactor(); // multiplication
-		                else if (eat('/')) x /= parseFactor(); // division
-		                else return x;
+		                if      (eat('*')) result *= parseFactor(); // multiplication
+		                else if (eat('/')) result /= parseFactor(); // division
+		                else return result;
 		            }
 		        }
 
@@ -121,31 +179,39 @@ public class GraphHandler {
 		            if (eat('+')) return parseFactor(); // unary plus
 		            if (eat('-')) return -parseFactor(); // unary minus
 
-		            double x;
-		            int startPos = this.pos;
+		            double result;
+		            int startPos = this.position;
 		            if (eat('(')) { // parentheses
-		                x = parseExpression();
+		                result = parseExpression();
 		                eat(')');
-		            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-		                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-		                x = Double.parseDouble(mathExpression.substring(startPos, this.pos));
-		            } else if (ch >= 'a' && ch <= 'z') { // functions
-		                while (ch >= 'a' && ch <= 'z') nextChar();
-		                String func = mathExpression.substring(startPos, this.pos);
-		                x = parseFactor();
-		                if (func.equals("sqrt")) x = Math.sqrt(x);
-		                else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-		                else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-		                else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+		            } else if ((aChar >= '0' && aChar <= '9') || aChar == '.') { // numbers
+		                while ((aChar >= '0' && aChar <= '9') || aChar == '.') nextChar();
+		                result = Double.parseDouble(mathExpression.substring(startPos, this.position));
+		            } else if (aChar >= 'a' && aChar <= 'z') { // functions
+		                while (aChar >= 'a' && aChar <= 'z') nextChar();
+		                String func = mathExpression.substring(startPos, this.position);
+		                result = parseFactor();
+		                if (func.equals("sqrt")) result = Math.sqrt(result);
+		                else if (func.equals("sin")) result = Math.sin(Math.toRadians(result));
+		                else if (func.equals("cos")) result = Math.cos(Math.toRadians(result));
+		                else if (func.equals("tan")) result = Math.tan(Math.toRadians(result));
 		                else throw new RuntimeException("Unknown function: " + func);
 		            } else {
-		                throw new RuntimeException("Unexpected: " + (char)ch);
+		                throw new RuntimeException("Unexpected: " + (char)aChar);
 		            }
 
-		            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+		            if (eat('^')) result = getExponentiation(result); 
 
-		            return x;
+		            return result;
 		        }
+
+				private double getExponentiation(double result) {
+					return Math.pow(result, parseFactor());
+				}
+
+				private void unexpectedCharError() {
+					throw new RuntimeException("Unexpected: " + (char)aChar);
+				}
 		    }.parse();
 		}
 
