34157a4d98e20e580bc8c5c85889cfb5a9cf0bc2
fnffn123 <37815222+fnffn123@users.noreply.github.com>
2020-05-31 21:49:18 +0900

    refactoring simpleinterest
    1. refactoring operations : rename , extract method, replace temp with query
    2. refactoring class : simpleinterest
    3. reason : to improve code understandability, to improve modifiability

--- Statistics/simpleinterest.java
+++ Statistics/simpleinterest.java
@@ -4,17 +4,39 @@ import java.util.Scanner;
 
 public class simpleinterest
 {
-    public static void main( String[] args)
-{
-    Scanner scn=new Scanner(System.in);
-    System.out.println("Enter Principle");
-    int p=scn.nextInt();
-    System.out.println("Enter Rate");
-    int r=scn.nextInt();
-    System.out.println("Enter Time");
-    int t=scn.nextInt();
-    int si=(p*r*t)/100;
-    System.out.println("Simple interest is " +si );
-    scn.close();
- }
+    public static void main(String[] args){
+	    
+	    printSimpleinterest( getSimpleInterest( getPrinciple(), getRate(), getTime() ) );
+	    
+	    }
+
+	private static void printSimpleinterest(int simpleinterest) {
+		System.out.println( "Simple interest is " + simpleinterest );
+	}
+
+    private static int getSimpleInterest(int principle, int rate, int time) {
+		int simpleinterest=(principle*rate*time)/100;
+		return simpleinterest;
+	}
+
+	private static int getTime() {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter Time");
+		int time=scn.nextInt();
+		return time;
+	}
+
+	private static int getRate() {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter Rate");
+		int rate=scn.nextInt();
+		return rate;
+	}
+
+	private static int getPrinciple() {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter Principle");
+		int principle=scn.nextInt();
+		return principle;
+	}
 } 
