5be68f44ad369c90a603e59ab87fc01076aaad97
fnffn123 <37815222+fnffn123@users.noreply.github.com>
2020-05-31 23:23:53 +0900

    refactoring GraphFrame
    1. refactoring operations : extract method
    2. refactoring method : GraphFrame
    3. reason : to improve code understandability

--- Graphing Calculator/GraphFrame.java
+++ Graphing Calculator/GraphFrame.java
@@ -7,13 +7,17 @@ public  class GraphFrame extends JFrame{
 
   public GraphFrame(GraphHandler handler){
 
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    InitializeGraphFrame(handler);
+    
+  }
+
+private void InitializeGraphFrame(GraphHandler handler) {
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setSize(1000, 1000);
     this.add((panel = new GraphPanel(handler)));
     this.setVisible(true);
     this.handler = handler;
-    
-  }
+}
 
   public GraphPanel getGraphPanel(){
     return panel;
