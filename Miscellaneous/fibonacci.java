e63047286c91c63ac847513bfb274dc309da6cff
fnffn123 <37815222+fnffn123@users.noreply.github.com>
2020-05-31 21:46:24 +0900

    refactoring fibonacci
    1. refactoring operations : rename , extract method, replace temp with query, extract variable
    2. refactoring class : fibonacci
    3. reason : to improve code understandability, to improve modifiability

--- Miscellaneous/fibonacci.java
+++ Miscellaneous/fibonacci.java
@@ -4,29 +4,57 @@ import java.util.Scanner;
 
 public class fibonacci {
     
-    public static boolean isFibo(int n){
-        double root1=sqrt(5*n*n-4);
-        double root2=sqrt(5*n*n+4);
-        if(root1%1==0 || root2%1==0)return true;  
-        return false;
+    public static boolean isFibonacci(int number){
+        boolean isFibo = (getRoot1(number)%1==0) || (getRoot2(number)%1==0);
+		return isFibo;
     }
+
+	private static double getRoot2(int number) {
+		return sqrt(5*number*number+4);
+	}
+
+	private static double getRoot1(int number) {
+		return sqrt(5*number*number-4);
+	}
     
     public static void main(String[] args) {
     
    try{
-        Scanner scan = new Scanner (System.in);
-        //Prompt user for a number 
-        System.out.println("Give a positive integer number: ");
-        int num=scan.nextInt(); 
-        if(num<0)throw new InputMismatchException();
-        if(isFibo(num))System.out.println(num+" is a Fibonacci number!");
-        else System.out.println(num+" is not a Fibonacci number!");
+        printResult(getNumber());
         
      }
-    catch (InputMismatchException ex){
-      System.err.println("Please provide only positive integer numbers (no text or negative numbers). Restart the program and try again!");
-      main(new String[0]);
+    catch (InputMismatchException exception){
+      printErrorMessage();
+      restartProgram();
     }
 }
+
+	private static int getNumber() {
+		Scanner scan = new Scanner (System.in);
+        System.out.println("Give a positive integer number: ");
+        int number=scan.nextInt(); 
+        checkNumber(number);
+		return number;
+	}
+
+	private static void checkNumber(int number) {
+		if(number<0)
+        	throw new InputMismatchException();
+	}
+
+	private static void printResult(int number) {
+		if(isFibonacci(number))
+        	System.out.println(number + " is a Fibonacci number!");
+        else 
+        	System.out.println(number + " is not a Fibonacci number!");
+	}
+
+	private static void restartProgram() {
+		main(new String[0]);
+	}
+
+	private static void printErrorMessage() {
+		System.err.println("Please provide only positive integer numbers (no text or negative numbers). Restart the program and try again!");
+	}
 
 }
