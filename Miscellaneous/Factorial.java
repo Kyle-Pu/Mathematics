d7d42219387e4209a9e81bb38eedc6cfa7ababcb
fnffn123 <37815222+fnffn123@users.noreply.github.com>
2020-05-31 21:12:44 +0900

    Update Factorial.java
    1. refactoring operations : rename , extract method, extract constant, extract variable
    2. refactoring class : Factorial
    3. reason : to improve code understandability, to improve modifiability

--- Miscellaneous/Factorial.java
+++ Miscellaneous/Factorial.java
@@ -9,26 +9,54 @@ import java.util.Scanner;
 
 public class Factorial {
 
-public static long facto(long num){
-    if(num<2)return 1;
-    return num*facto(num-1);
-}
-    
-    
-public static void main(String[] args){
-    try{
-        Scanner scan = new Scanner (System.in);
-    
-    //Prompt user for a number to calculte its factorial number
-    System.out.println("Insert a positive integer number: ");
-    int num = scan.nextInt();
-    if(num<0)throw(new InputMismatchException());
-    long F=facto(num);
-        System.out.println("Factorial number of "+num+" is " +F);
-    }
-    catch (InputMismatchException ex){
-      System.err.println("Please provide a positive integer number (no text, real or negative number). Restart the program and try again!");
-      main(new String[0]);
-    }
-}
+	private static final int MINIMUM_NUMBER = 0;
+
+	public static long getFactorial(long number){
+	    if( number < 2 ) 
+	    	return 1;
+	    long factorial = number * getFactorial( number - 1 );
+		return factorial;
+	}
+	
+	public static void main(String[] args){
+	    try{
+	        int number = getNumber();
+		    
+		    printResult( number, getFactorial(number) );
+	    }
+	    catch (InputMismatchException exception){
+	      printErrorMessage();
+	      restartProgram();
+	    }
+	}
+
+	private static void checkNumber(int number) {
+		if( number < MINIMUM_NUMBER )
+			inputError();
+	}
+
+	private static void inputError() {
+		throw( new InputMismatchException() );
+	}
+
+	private static void printResult(int number, long result) {
+		System.out.println("Factorial number of "+number+" is " +result);
+	}
+
+	private static void restartProgram() {
+		main(new String[0]);
+	}
+
+	private static void printErrorMessage() {
+		System.err.println("Please provide a positive integer number (no text, real or negative number). Restart the program and try again!");
+	}
+	
+	private static int getNumber() {
+		Scanner scan = new Scanner (System.in);
+
+		System.out.println("Insert a positive integer number: ");
+		int number = scan.nextInt();
+        checkNumber(number);
+		return number;
+	}
 }
